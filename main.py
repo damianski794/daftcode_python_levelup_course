5ec5b39030112bfb008bb21f3af0c39eae6b31dd
Damian <d.wysokinski@versabox.pl>
2020-04-27 01:01:02 +0200

    wyklad 3 zad. 5

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import Cookie, Depends, FastAPI, HTTPException, status
+from fastapi import Cookie, Depends, FastAPI, HTTPException, Response, status
 from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+
+from typing import Dict
 
 from pydantic import BaseModel
 #from .routers import users #todo przerobic zeby odwolywac sie do tego usera z folderu .routers
@@ -68,26 +71,27 @@ def DELETEmethod():
 
 class patient(BaseModel):
     name: str
-    surename: str
+    surname: str
 
 class patient_simple_response(BaseModel):
     id: int
     patient: patient
 
-patients = []
+patients: Dict[str, patient] = {}
 
-@app.post('/patient', response_model=patient_simple_response)
-def patient_simple_poster(patient_rq: patient):
-    global patients_counter
-    patients.append(patient_simple_response(id=len(patients), patient=patient_rq))
-    return patient_simple_response(id=(len(patients)-1), patient=patient_rq)
+# zakomentowane 26.04
+# @app.post('/patient', response_model=patient_simple_response, dependencies = [Depends(users.check_if_session_exists)])
+# def patient_simple_poster(patient_rq: patient):
+#     global patients_counter
+#     patients.append(patient_simple_response(id=len(patients), patient=patient_rq))
+#     return patient_simple_response(id=(len(patients)-1), patient=patient_rq)
 
 # Zadanie 4
 @app.get('/patient/{number}')
 def patient_simple_getter(number: int):
     global patients
     if number < len(patients) and number >= 0:
-        return patients[number].patient
+        return patients.get(str(number)) #.patient
     raise HTTPException(status_code=204, detail="No content")
 
 
@@ -106,3 +110,40 @@ def welcome_user(request: Request, cookie: str = Depends(users.check_if_session_
     if cookie not in users.set_of_session_tokens:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorised, jest lipa z ciasteczkiem")
     return templates.TemplateResponse("item.html", {"request": request, "user": "trudnY"})
+
+
+@app.post('/patient', response_model=patient)
+def add_new_patient(name: str, surname: str, response: Response, token: str = Depends(users.check_if_session_exists)):
+    patient_ = patient(name=name, surname=surname)
+    id_ = app.counter
+    patients.update({id_: {"name": patient_.name,
+                           "surname": patient_.surname}})
+
+    response.status_code = status.HTTP_301_MOVED_PERMANENTLY
+    response.headers["Location"] = f"/patient/{id_}"
+    # print('---------------')
+    # print(f"patients= {patients}")
+    # print('---------------')
+
+    app.counter += 1
+
+    return patient_
+
+@app.get('/patient', dependencies = [Depends(users.check_if_session_exists)])
+def get_all_patients():
+    # print(patients)
+    return patients
+
+@app.delete('/patient/{id}', dependencies = [Depends(users.check_if_session_exists)])
+def remove_patient(response: Response ,id: int):
+    # print('patients przed usuwaniem', patients)
+    if id in patients.keys():
+        # print(f'id={id} jest w patients keys')
+        patients.pop(id, None)
+        response.status_code = status.HTTP_204_NO_CONTENT
+
+    # print('patients po usuwaniu', patients)
+
+
+
+
